cc96f0fb9d4825f69fe09b28aec14636a43be609
MartinOfi <martinoficialdegui@hotmail.com>
2021-09-18 12:53:45 -0300

    Challenge 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
 import '../styles/globals.css'
+import { CartProvider } from './public/Context.jsx'
+import client from './public/Context.jsx/client'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
+  return (
+    <ApolloProvider client={client}>
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
+  </ApolloProvider>
+  )}
 export default MyApp
