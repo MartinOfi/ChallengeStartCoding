d5caeae5cf83f85821f9c01778dc95516ae3732c
MartinOfi <martinoficialdegui@hotmail.com>
2021-09-23 15:59:50 -0300

    Hay problemas con el estado de la estrella en los favoritos

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,43 +1,21 @@
 import { useEffect, useState } from "react";
+import { FavoriteStar } from "./favoriteStar";
 
 const Card = ({ item, handleAddFavorite }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
-  useEffect(() => {
-    const favoritesStorage = JSON.parse(
-      window.localStorage.getItem("favorites")
-    );
-    if(favoritesStorage.find(el => el.id == item.id)){
-      setIsFavorite(true)
-    }    
-  }, []);
-
   return (
     <div className="card shadow-sm m-3">
       {item.hasOwnProperty("image") && (
         <div className="image-icon">
           <img src={item.image} className="card-img-top" alt="" />
-          <div className="star">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              fill="rgb(169,212,245)"
-              className="bi bi-star-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-            </svg>
-          </div>
+            <FavoriteStar
+              item={item}
+              handleAddFavorite={handleAddFavorite}
+            />
         </div>
       )}
       <div className="card-body">
         <h5 className="card-title">{item.name}</h5>
-        <button
-          className="btn btn-light"
-          onClick={() => handleAddFavorite(item)}
-        >
-          See More
-        </button>
+        <button className="btn btn-light">See More</button>
       </div>
     </div>
   );
