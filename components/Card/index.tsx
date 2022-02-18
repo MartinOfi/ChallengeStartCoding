5db98641b53c7b33bb91dff49abf5f1ea44df14d
RaCaTaSh <racatashsp@gmail.com>
2022-02-18 11:49:35 -0300

    mejora de estilos

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,39 +1,40 @@
 import { FavoriteStar } from "./favoriteStar";
 import Link from "next/link";
 interface Props {
-  item:any;
-  page?:string;
-  option:string;
+  item: any;
+  page?: string;
+  option: string;
 }
-const Card = ({ item, page = "", option }:Props) => {
+const Card = ({ item, page = "", option }: Props) => {
   return (
-    <div
-      className="shadow-sm m-3 cursor-pointer card-total"
-      style={{ cursor: "pointer" }}
+    <Link
+      scroll={false}
+      href={{
+        pathname: `/Details/${item.id}`,
+        query: {
+          type: option,
+        },
+      }}
     >
-      {item.hasOwnProperty("image") && (
-        <div className="image-icon">
-          <img src={item.image} className="card-img-top" alt="" />
-          <FavoriteStar item={item} page={page} />
-        </div>
-      )}
-      <div className="card-body">
-        <h5 className="card-title">{item.name}</h5>
-        <Link
-          scroll={false}
-          href={{
-            pathname: `/Details/${item.id}`,
-            query: {
-              type: option,
-            },
-          }}
-        >
+      <div
+        className="shadow-sm m-3 cursor-pointer card-total"
+        style={{ cursor: "pointer" }}
+      >
+        {item.hasOwnProperty("image") && (
+          <div className="image-icon">
+            <img src={item.image} className="card-img-top" alt="" />
+            <FavoriteStar item={item} page={page} />
+          </div>
+        )}
+        <div className="card-body">
+          <h5 className="card-title">{item.name}</h5>
+
           <a className="btn btn-primary" role="button">
             Details
           </a>
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 export default Card;
